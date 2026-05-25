79b5dac704e524aa3177d8fce0cab34fc4a7972c
marco-lopiano <marco.lopiano.95@gmail.com>
2026-05-25 17:28:16 +0200

    Cleanup: removed oldFilter method, useless import and lines

--- src/filtering.py
+++ src/filtering.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
 
-from utils import misc
-
 #Inplace waiting for a proper fix
 import warnings
 warnings.simplefilter('ignore')
@@ -40,66 +38,6 @@ def _filterMostRecent(database):
     out = database.loc[mask]
 
     return out
-
-# TODO: remove this
-def oldFilter(database, filters=[], columnsToFilter=[]):
-    """
-    Old method of filtering
-
-    Args:
-        - Pandas.Dataframe
-    Optional args:
-        - filters list of str
-        - columnsToFilter list of str
-    Returns:
-        - Pandas.Dataframe
-    """
-
-    if len(columnsToFilter) == 0:
-        return database
-    
-    elif len(columnsToFilter) == 1:
-        columnsToFilter = columnsToFilter[0]
-        if len(filters)>0:
-            filtered = pd.DataFrame(columns=database.columns)
-            for f in filters:
-
-                filtering = database[columnsToFilter].str.lower().str.contains(f)
-                tmp = database.loc[filtering]
-
-                filtered = filtered._append(tmp.dropna())
-
-            if filtered.size > 0:
-                return filtered
-            else:
-                return database
-        else:
-            return database
-        
-    elif len(columnsToFilter) > 1:
-        filtered_df = pd.DataFrame(columns=database.columns)
-        filter_pairs = misc.simplePairing(columnsToFilter, filters)
-
-        for pair in filter_pairs:
-
-            col_a_value, cell_a_value, col_b_value, cell_b_value = pair[0][0], pair[0][1], pair[1][0], pair[1][1]
-
-            tmp_filtered = database[(database[col_a_value].str.lower().str.contains(cell_a_value)) & (database[col_b_value].str.lower().str.contains(cell_b_value))]
-            
-            if tmp_filtered.size > 0:
-                filtered_df = filtered_df._append(tmp_filtered)
-            else:
-                pass
-            
-            if filtered_df.size > 0:
-                return filtered_df
-            else:
-                print('nothing found')
-                return database
-    else:
-        return database
-
-    return 
 
 def advancedFilter(database, filters=[], columnsToFilter=[], mostRecent=False):
     """
@@ -157,14 +95,5 @@ def advancedFilter(database, filters=[], columnsToFilter=[], mostRecent=False):
 
 if __name__ == "__main__":
     import database
-
     t = database.main()
     print(_dateOrdering(t))
-
-
-
-
-    # filters = ['tech', 'light']
-    # columnsToFilter = ['Job Title']
-
-    # print(advancedFilter(t, filters=filters, columnsToFilter=columnsToFilter, mostRecent=True))
